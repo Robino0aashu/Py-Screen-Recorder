e6ce83f390b7e6c85f4c58ea594b8bc8aae831dd
Robino0aashu <aashu.20404kr@gmail.com>
2023-08-25 15:35:14 +0530

    code update

--- screenRec.py
+++ screenRec.py
@@ -1,7 +1,7 @@
-from tkinter import *
+import tkinter as tk
 import pyscreenrec
 
-scr=Tk()
+scr=tk.Tk()
 
 scr.geometry("400x600")
 scr.title("Screen Recorder")
@@ -31,44 +31,44 @@ def stopRec():
 rec=pyscreenrec.ScreenRecorder()
 
 #icon
-image_icon=PhotoImage(file="icon.png")
+image_icon=tk.PhotoImage(file="icon.png")
 scr.iconphoto(False, image_icon)
 
 #bg image
-image1=PhotoImage(file="yelllow.png")
-Label(scr, image=image1, bg="#fff").place(x=-2, y=35)
+image1=tk.PhotoImage(file="yelllow.png")
+tk.Label(scr, image=image1, bg="#fff").place(x=-2, y=35)
 
-image2=PhotoImage(file="blue.png")
-Label(scr, image=image2, bg="#fff").place(x=223, y=200)
+image2=tk.PhotoImage(file="blue.png")
+tk.Label(scr, image=image2, bg="#fff").place(x=223, y=200)
 
 #Headline
-lbl=Label(scr, text="Py Screen recorder", bg="#fff", font="arial 15 bold")
+lbl=tk.Label(scr, text="Py Screen recorder", bg="#fff", font="arial 15 bold")
 lbl.pack(pady=10)
 
-image3=PhotoImage(file="recording.png")
-Label(scr, image=image3, bd=5).pack(pady=30)
+image3=tk.PhotoImage(file="recording.png")
+tk.Label(scr, image=image3, bd=5).pack(pady=30)
 
 #Entry-Name
-Filename=StringVar()
-entry=Entry(scr, textvariable=Filename, width=18, font="arial 15")
+Filename=tk.StringVar()
+entry=tk.Entry(scr, textvariable=Filename, width=18, font="arial 15")
 entry.place(x=72, y=350)
 Filename.set("Enter a name")
 
 
 #buttons
-start=Button(scr, text="Start", font="arial 20", bd=0, command=startRec)
+start=tk.Button(scr, text="Start", font="arial 20", bd=0, command=startRec)
 start.place(x=135, y=235)
 
-image4=PhotoImage(file="pause.png")
-pause=Button(scr, image=image4, bd=0, bg="#fff", command=pauseRec)
+image4=tk.PhotoImage(file="pause.png")
+pause=tk.Button(scr, image=image4, bd=0, bg="#fff", command=pauseRec)
 pause.place(x=50, y=450)
 
-image5=PhotoImage(file="resume.png")
-resume=Button(scr, image=image5, bd=0, bg="#fff", command=resumeRec)
+image5=tk.PhotoImage(file="resume.png")
+resume=tk.Button(scr, image=image5, bd=0, bg="#fff", command=resumeRec)
 resume.place(x=150, y=450)
 
-image6=PhotoImage(file="stop.png")
-stop=Button(scr, image=image6, bd=0, bg="#fff", command=stopRec)
+image6=tk.PhotoImage(file="stop.png")
+stop=tk.Button(scr, image=image6, bd=0, bg="#fff", command=stopRec)
 stop.place(x=250, y=450)
 
 
